b12688d642c07d2cc8a2c66a831a739419e440c1
Yoed Halberstam <yoedha@mta.ac.il>
2025-06-12 16:04:19 +0300

    S

---
+++ Ex05.MasterMindWin/GameForm.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ex05.MasterMindWin
+{
+    public partial class GameForm : Form
+    {
+        private Color m_Color;
+        private int m_CodeCount = 4;
+        private int m_NumOfGuesses;
+        private List<Color> m_Colors = new List<Color>
+        {
+            Color.Purple,
+            Color.Yellow,
+            Color.Brown,
+            Color.Green,
+            Color.Blue,
+            Color.White,
+            Color.Red,
+            Color.LightBlue
+        };
+
+        public Color Color
+        {
+            get
+            {
+                return m_Color;
+            }
+        }
+
+        public GameForm()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
